99a930288bed20ae0e4121907495ab784ae1590b
ClinkMakesGames <32272073+ClinkMakesGames@users.noreply.github.com>
2018-06-17 12:48:18 +0100

    Added purge command

---
+++ commands/admin/purge.js
@@ -0,0 +1,27 @@
+exports.process = function(info) {
+  var num = parseInt(info.args[0]);
+  if (!num) {
+    info.message.reply('the correct usage of this command is `' + info.config.prefix + 'purge <number>`')
+      .then((msg) => msg.delete(info.config.deleteAfter))
+      .catch(info.logger.logError);
+    return;
+  }
+  if (num < 1 || num > 100) {
+    info.message.reply('please choose an integer between 1 and 100')
+      .then((msg) => msg.delete(info.config.deleteAfter))
+      .catch(info.logger.logError);
+    return;
+  }
+
+  info.message.channel.send('Deleting the last **' + num + '** messages...')
+    .then(msg => {
+      msg.delete(info.config.deleteAfter)
+        .then(() => {
+          info.message.channel.bulkDelete(num, false)
+            .catch(info.logger.logError);
+        });
+    })
+    .catch(info.logger.logError);
+}
+
+exports.usage = 'purge <number> | purge the last n messages';

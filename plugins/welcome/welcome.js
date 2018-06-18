674ca2e0ae57849bb30ae31b5fe86cec88dc096c
ClinkMakesGames <32272073+ClinkMakesGames@users.noreply.github.com>
2018-06-18 18:06:17 +0100

    Added option to assign roles to new guild members

--- plugins/welcome/welcome.js
+++ plugins/welcome/welcome.js
@@ -8,6 +8,17 @@ exports.init = function(i) {
 }
 
 function welcome(member) {
+  for (var i = 0; i < lConfig.roles.length; i++) {
+    var role = member.guild.roles.find('name', lConfig.roles[i]);
+
+    if (!role) {
+      info.logger.logWarning('\'' + lConfig.roles[i] + '\', a role specified in welcome.json could not be found.');
+    }
+    else {
+      member.addRole(role);
+    }
+  }
+
   if (!lConfig.message || lConfig.message == "")
     return;
 
